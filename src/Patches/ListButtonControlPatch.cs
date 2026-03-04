9633e38a17f5646f36ed238cb30cf9e4681971f6
renshareck <renshareck@foxmail.com>
2026-03-04 14:05:06 +0800

    修改按钮的pading.bottom为1（翻译后文本按钮中UITextBlock自带1px的padding）

---
+++ src/Patches/ListButtonControlPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using TranslationMod.Configuration;
+
+namespace TranslationMod.Patches
+{
+    [HarmonyPatch(typeof(ListButtonControl), "createButton")]
+    public static class ListButtonControlPatch
+    {
+        [HarmonyPostfix]
+        private static void Postfix(object __result)
+        {
+            if (__result is UIElement button)
+            {
+                if (LanguageManager.NoLetterLanguage())
+                {
+                    var padding = button.padding;
+                    padding.bottom = 1;
+                    button.padding = padding;
+                }
+            }
+        }
+    }
+}
